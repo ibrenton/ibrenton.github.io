108953b32f59f6219f832a823aa2803f520ee7da
ibrenton <34819680+ibrenton@users.noreply.github.com>
2019-06-30 02:32:00 -0500

    Added new function to main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function main() {
-    document.write('main success');
+    document.write('Big Time success, you\'re king of the castle!');
+}
+
+function discourage() {
+    document.write('You will never get this, you will never get this');
 }
