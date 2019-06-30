e449fa13f425ec2e6b9da366032d2dc050b3f93f
ibrenton <34819680+ibrenton@users.noreply.github.com>
2019-06-30 04:21:48 -0500

    created getBTCprice()

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-function main() {
-    document.write('Big Time success, you\'re king of the castle!');
-}
+var formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
 
-function discourage() {
-    document.write('You will never get this, you will never get this');
-}
 
-function encourage() {
-    document.write('You\'re ALL breathtaking! -The timeless vampire');
+// Change getBTCprice() to getCryptoPrice(): just make url variable based on a parameter, making this function reusable internally
+
+function getBTCprice() {
+    const url = 'https://api.coinbase.com/v2/prices/BTC-USD/spot';
+    const priceDisplay = document.getElementById("output");
+    const info = {
+        method: "GET"
+    }
+    fetch(url, info)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            console.log(JSON.stringify(myJson));
+            console.log('Price: $', myJson.data.amount);
+            let price = formatter.format(myJson.data.amount);
+            priceDisplay.innerHTML = price;
+        });
 }
